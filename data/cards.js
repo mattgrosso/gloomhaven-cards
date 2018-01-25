d1c51f17eb46df8e5dac48c2edb5dc1ae29c82f9
mattgrosso <mattgrosso@gmail.com>
2018-01-25 12:10:32 -0500

    Edit data arrays and add decksInUse

--- data/cards.js
+++ data/cards.js
@@ -1,4 +1,4 @@
-var decks = [
+const DECKS = [
   {
     class: "Ancient Artillery",
     cards: [
--- data/monsters.js
+++ data/monsters.js
@@ -1,142 +1,142 @@
-var monsters = {
-  "Ancient Artillery": {
+var monsters = [
+  {
     name: "Ancient Artillery",
     class: "Ancient Artillery"
   },
-  "Bandit Archer": {
+  {
     name: "Bandit Archer",
     class: "Archer"
   },
-  "Bandit Guard": {
+  {
     name: "Bandit Guard",
     class: "Guard"
   },
-  "Black Imp": {
+  {
     name: "Black Imp",
     class: "Imp"
   },
-  "Boss": {
+  {
     name: "Boss",
     class: "Boss"
   },
-  "Cave Bear": {
+  {
     name: "Cave Bear",
     class: "Cave Bear"
   },
-  "City Archer": {
+  {
     name: "City Archer",
     class: "Archer"
   },
-  "City Guard": {
+  {
     name: "City Guard",
     class: "Guard"
   },
-  "Cultist": {
+  {
     name: "Cultist",
     class: "Cultist"
   },
-  "Deep Terror": {
+  {
     name: "Deep Terror",
     class: "Deep Terror"
   },
-  "Earth Demon": {
+  {
     name: "Earth Demon",
     class: "Earth Demon"
   },
-  "Flame Demon": {
+  {
     name: "Flame Demon",
     class: "Flame Demon"
   },
-  "Forest Imp": {
+  {
     name: "Forest Imp",
     class: "Imp"
   },
-  "Frost Demon": {
+  {
     name: "Frost Demon",
     class: "Frost Demon"
   },
-  "Giant Viper": {
+  {
     name: "Giant Viper",
     class: "Giant Viper"
   },
-  "Harrower Infester": {
+  {
     name: "Harrower Infester",
     class: "Harrower Infester"
   },
-  "Hound": {
+  {
     name: "Hound",
     class: "Hound"
   },
-  "Inox Archer": {
+  {
     name: "Inox Archer",
     class: "Archer"
   },
-  "Inox Guard": {
+  {
     name: "Inox Guard",
     class: "Guard"
   },
-  "Inox Shaman": {
+  {
     name: "Inox Shaman",
     class: "Shaman"
   },
-  "Living Bones": {
+  {
     name: "Living Bones",
     class: "Living Bones"
   },
-  "Living Corpse": {
+  {
     name: "Living Corpse",
     class: "Living Corpse"
   },
-  "Living Spirit": {
+  {
     name: "Living Spirit",
     class: "Living Spirit"
   },
-  "Lurker": {
+  {
     name: "Lurker",
     class: "Lurker"
   },
-  "Night Demon": {
+  {
     name: "Night Demon",
     class: "Night Demon"
   },
-  "Ooze": {
+  {
     name: "Ooze",
     class: "Ooze"
   },
-  "Rending Drake": {
+  {
     name: "Rending Drake",
     class: "Rending Drake"
   },
-  "Savvas Icestorm": {
+  {
     name: "Savvas Icestorm",
     class: "Savvas Icestorm"
   },
-  "Savvas Lavaflow": {
+  {
     name: "Savvas Lavaflow",
     class: "Savvas Lavaflow"
   },
-  "Spitting Drake": {
+  {
     name: "Spitting Drake",
     class: "Spitting Drake"
   },
-  "Stone Golem": {
+  {
     name: "Stone Golem",
     class: "Stone Golem"
   },
-  "Sun Demon": {
+  {
     name: "Sun Demon",
     class: "Sun Demon"
   },
-  "Vermling Scout": {
+  {
     name: "Vermling Scout",
     class: "Scout"
   },
-  "Vermling Shaman": {
+  {
     name: "Vermling Shaman",
     class: "Shaman"
   },
-  "Wind Demon": {
+  {
     name: "Wind Demon",
     class: "Wind Demon"
   }
-};
+];
