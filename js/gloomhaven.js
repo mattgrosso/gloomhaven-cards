4ea56497dbb28dc47a0db0cd0e3c708f00efcca3
mattgrosso <mattgrosso@gmail.com>
2018-01-25 12:11:48 -0500

    Enable random card draw

--- js/gloomhaven.js
+++ js/gloomhaven.js
@@ -4,51 +4,88 @@
 
   // If there is already a monster selected, this function calls on draw card.
   // Otherwise, it toggles the monster select modal.
-  $('.monster-card img').click(function (event) {
-    var currentDeck = $(event.target).closest('.monster-card');
-    if (!currentDeck.attr('data-monster')) {
+  $('.monster img').click(function (event) {
+    var currentMonster = $(event.target).closest('.monster');
+    if (!currentMonster.attr('data-monster')) {
       $('.monster-select-modal').hide();
-      currentDeck.find('.monster-select-modal').toggle();
+      currentMonster.find('.monster-select-modal').toggle();
     } else {
-      console.log('Draw a card');
+      drawCard(currentMonster);
     }
   })
 
-  // This function should listen for a selection on the modal.
+  // This function listens for a selection on the modal.
   $('.monster-select-modal li').click(function (event) {
     setDeck(event);
   });
 
   // This function listens for clicks on the clear button.
   $('.clear-monster').click(function (event) {
-    clearMonster(event.target.closest('.monster-card'));
+    clearMonster(event.target.closest('.monster'));
   })
 
   // This function clears the current monster type from a deck.
-  function clearMonster(targetDeck) {
-    $(targetDeck).attr('data-monster', '');
-    $(targetDeck).find('h2').text('Click to Select Monster');
-    $(targetDeck).find('.clear-monster').hide();
+  function clearMonster(targetMonster) {
+    delete decksInUse[targetMonster.id];
+    $(targetMonster).attr('data-monster', '');
+    $(targetMonster).find('h2').text('Click to Select Monster');
+    $(targetMonster).find('.clear-monster').hide();
   }
 
   // This function should apply the chosen monster to the correct deck.
+  // The deck is held in 'decksInUse' under it's 'monster-number' ID.
   function setDeck(event) {
-    var monsterSelection = event.target.dataset.value;
-    console.log(event);
-    var currentDeck = $(event.target).closest('.monster-card');
-    currentDeck.attr('data-monster', monsterSelection);
-    currentDeck.find('h2').text(monsterSelection);
-    currentDeck.find('.clear-monster').show();
+    var monsterClass = event.target.dataset.value;
+    var currentMonster = $(event.target).closest('.monster');
+    var monsterDeckIndex = findMonsterDeckIndex(monsterClass);
+
+    decksInUse[currentMonster[0].id] = {
+      class: monsterClass,
+      index: monsterDeckIndex,
+      remainingValues: [0, 1, 2, 3, 4, 5, 6, 7]
+    };
+
+    currentMonster.attr('data-monster', monsterClass);
+    currentMonster.find('h2').text(monsterClass);
+    currentMonster.find('.clear-monster').show();
     $('.monster-select-modal').hide();
+  }
+
+  // This function should look at the deck that was clicked and reveal a random
+  // card from that deck's remaining cards.
+  function drawCard(currentMonster) {
+    var currentDeck = decksInUse[currentMonster[0].id];
+    var randomIndex = Math.floor(Math.random() * currentDeck.remainingValues.length);
+    var randomValue = currentDeck.remainingValues.splice(randomIndex, 1);
+    var randomCard = DECKS[currentDeck.index].cards[randomValue];
+
+    if (randomCard[0]) {
+      currentDeck.remainingValues = [0, 1, 2, 3, 4, 5, 6, 7]
+    }
+    
+    return randomCard;
+  }
+
+  // This function takes in a monster class and returns the index value for the
+  // appropriate monster deck object from the data.
+  function findMonsterDeckIndex(monsterClass) {
+    let monsterDeckIndex;
+
+    DECKS.forEach(function (each, index) {
+      if (each.class === monsterClass) {
+        monsterDeckIndex = index;
+      }
+    })
+
+    return monsterDeckIndex;
   }
 
   // This function generates the list of monsters from the data.
   function generateOptions(data) {
-    console.log('This is a flag');
     var ul = $('.monster-select-modal ul');
 
-    $.each(data, function (each) {
-      var li = $('<li/>').attr('data-value', each).text(each).appendTo(ul);
+    data.forEach(function (each) {
+      var li = $('<li/>').attr('data-value', each.class).text(each.name).appendTo(ul);
     })
   }
 })();
